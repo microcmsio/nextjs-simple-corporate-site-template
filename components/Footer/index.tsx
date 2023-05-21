afd6843da534318d960f60ad279498df79ea39ce
Kazuki Shibata <shibata@microcms.co.jp>
2023-05-21 09:46:27 +0900

    [update] footerにナビゲーション追加

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,8 +1,25 @@
+import Link from 'next/link';
 import styles from './index.module.css';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
+      <nav className={styles.nav}>
+        <ul className={styles.items}>
+          <li className={styles.item}>
+            <Link href="/news">ニュース</Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="/members">メンバー</Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="/recruit">採用情報</Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="/contact">お問い合わせ</Link>
+          </li>
+        </ul>
+      </nav>
       <p className={styles.cr}>© SIMPLE. All Rights Reserved 2023</p>
     </footer>
   );
