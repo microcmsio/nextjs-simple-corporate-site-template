cdf3529bf46deab273a5c6523686f3b2199c72ad
Kazuki Shibata <shibata@microcms.co.jp>
2023-05-24 15:29:34 +0900

    [update] メンバーが0件時の対応

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -9,24 +9,28 @@ export default async function Page() {
   const data = await getMembersList();
   return (
     <div className={styles.container}>
-      <ul>
-        {data.contents.map((member) => (
-          <li key={member.id} className={styles.list}>
-            <Image
-              src={member.image?.url as string}
-              alt=""
-              width={member.image?.width}
-              height={member.image?.height}
-              className={styles.image}
-            />
-            <dl>
-              <dt className={styles.name}>{member.name}</dt>
-              <dd className={styles.position}>{member.position}</dd>
-              <dd className={styles.profile}>{member.profile}</dd>
-            </dl>
-          </li>
-        ))}
-      </ul>
+      {data.contents.length > 0 ? (
+        <p className={styles.empty}>メンバーが登録されていません。</p>
+      ) : (
+        <ul>
+          {data.contents.map((member) => (
+            <li key={member.id} className={styles.list}>
+              <Image
+                src={member.image?.url as string}
+                alt=""
+                width={member.image?.width}
+                height={member.image?.height}
+                className={styles.image}
+              />
+              <dl>
+                <dt className={styles.name}>{member.name}</dt>
+                <dd className={styles.position}>{member.position}</dd>
+                <dd className={styles.profile}>{member.profile}</dd>
+              </dl>
+            </li>
+          ))}
+        </ul>
+      )}
       <div className={styles.footer}>
         <h2 className={styles.message}>We are hiring</h2>
         <p>私たちは共にチャレンジする仲間を募集しています。</p>
